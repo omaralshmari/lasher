79f84c6f3c6c34b6a5e3ff6da698d8bf82b6f718
omaralshmari <41575972+omaralshmari@users.noreply.github.com>
2018-08-10 22:56:06 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,20 +1,19 @@
 const Discord = require('discord.js');
-const A7MD = new Discord.Client();
+const client = new Discord.Client();
 
-console.log("BOT ONLINE");
+console.log("Welcome Savage.");
  
-A7MD.on("guildMemberAdd", member => {
+client.on("guildMemberAdd", member => {
   member.createDM().then(function (channel) {
-  return channel.send(` 
-**
+  return channel.send(`**
 السلام عليكم ياعيني..
 ابيك بموضوع مهم هنا اذا ما عليك أمر !
 
-  [ https://discord.gg/HunzvKc ] 
+  https://discord.gg/tagSsfA
 
-الدعووة خاصة لك ... [ ${member}  ]
-**`) 
+ **`) 
+
 }).catch(console.error)
 })
 
-client.login(process.env.BOT_TOKEN); 
+client.login("NDcwOTgwNTIzOTU5Nzc5MzM4.Dk9tPw.40vaXaIlQGDvLaYlrPEQ2jfbQes"); 
